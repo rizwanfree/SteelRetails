4e0e7460ef1d64741d5b0d297239bc2063ad9f61
rizwanfree <121812303+rizwanfree@users.noreply.github.com>
2024-05-09 16:43:30 +0500

    setting up main template form for actions

---
+++ SteelRetails/Screens/MainTemplateForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SteelRetails.Screens
+{
+    partial class MainTemplateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topBorderPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // topBorderPanel
+            // 
+            this.topBorderPanel.BackColor = System.Drawing.Color.DarkGray;
+            this.topBorderPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topBorderPanel.Location = new System.Drawing.Point(0, 0);
+            this.topBorderPanel.Name = "topBorderPanel";
+            this.topBorderPanel.Size = new System.Drawing.Size(800, 45);
+            this.topBorderPanel.TabIndex = 0;
+            this.topBorderPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.topBorderPanel_MouseDown);
+            this.topBorderPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.topBorderPanel_MouseMove);
+            // 
+            // MainTemplateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.topBorderPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainTemplateForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel topBorderPanel;
+    }
+}
---
+++ SteelRetails/Screens/MainTemplateForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteelRetails.Screens
+{
+    public partial class MainTemplateForm : Form
+    {
+        public Point MouseLocation;
+        public bool IsUpdate { get; set; }
+        public string Company { get; set; } = "ABC Company";
+        public string Address1 { get; set; } = "123 Lane";
+        public string Address2 { get; set; } = "Iron Area, CityName";
+        public string Phone { get; set; } = "021-1234567";
+        public string Email { get; set; } = "abc@xyz.com";
+        public MainTemplateForm()
+        {
+            InitializeComponent();
+        }
+
+        private void topBorderPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.MouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private void topBorderPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePoint = Control.MousePosition;
+                mousePoint.Offset(MouseLocation.X, MouseLocation.Y);
+                Location = mousePoint;
+            }
+        }
+    }
+}
